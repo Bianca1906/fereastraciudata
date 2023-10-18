1a6a125824fd7e8d060ab10d5079b47f1b2ad9b3
Bianca1906 <87058419+Bianca1906@users.noreply.github.com>
2023-10-18 09:52:33 +0300

    Add files via upload

---
+++ fereastraciudata/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fereastraciudata
+{
+    public partial class Form1 : Form
+    {
+        Form2 f;
+        public Form1()
+        {
+            InitializeComponent();
+           
+
+        }
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                // Convert HTCLIENT to HTCAPTION
+                if (m.Result.ToInt32() == HTCLIENT)
+                {
+                    m.Result = (IntPtr)HTCAPTION;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+       
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+             f = new Form2();
+            f.Show();
+            f.Left = 1200;
+            f.Top =200;
+            timer1.Enabled = true;
+        
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+            Form2.ActiveForm.Close();
+            Form1.ActiveForm.Close();
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            f.Left+=10;
+        }
+    }
+}
---
+++ fereastraciudata/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fereastraciudata
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private const int WM_NCHITTEST = 0x84;
+        private const int HTCLIENT = 0x1;
+        private const int HTCAPTION = 0x2;
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                // Convert HTCLIENT to HTCAPTION
+                if (m.Result.ToInt32() == HTCLIENT)
+                {
+                    m.Result = (IntPtr)HTCAPTION;
+                }
+            }
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
